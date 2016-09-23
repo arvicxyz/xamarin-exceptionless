dfc9a44fc683010d3d920936e042d9821270bae3
arvicxyz <dudeamirfahd@gmail.com>
2016-09-24 00:09:43 +0800

    Apply Exceptionless on Xamarin.iOS

--- XamarinExceptionless/iOS/Main.cs
+++ XamarinExceptionless/iOS/Main.cs
@@ -1,4 +1,6 @@
-﻿using UIKit;
+﻿using System;
+using System.Diagnostics;
+using UIKit;
 
 namespace XamarinExceptionless.iOS
 {
@@ -6,7 +8,22 @@ namespace XamarinExceptionless.iOS
     {
         static void Main(string[] args)
         {
-            UIApplication.Main(args, null, "AppDelegate");
+            try
+            {
+                // Initialize Exceptionless client before app startup
+                ExceptionlessHelper.Initialize("your_api_key_here");
+
+                // Entry point of your Xamarin.iOS application
+                UIApplication.Main(args, null, "AppDelegate");
+            }
+            catch (Exception ex)
+            {
+                // Submit an exception to the Exceptionless server
+                ExceptionlessHelper.SubmitException(ex);
+
+                // Display error message for local debugging purposes
+                Debug.WriteLine(ex);
+            }
         }
     }
 }
