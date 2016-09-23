c759ef5792532755b5925dd933ee9d34367fd472
arvicxyz <dudeamirfahd@gmail.com>
2016-09-24 00:39:57 +0800

    Apply Exceptionless on Xamarin.Android

---
+++ XamarinExceptionless/Droid/CustomExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using Java.Lang;
+
+namespace XamarinExceptionless.Droid
+{
+    public class CustomExceptionHandler : Thread.IUncaughtExceptionHandler
+    {
+        public CustomExceptionHandler()
+        {
+        }
+
+        public IntPtr Handle
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UncaughtException(Thread t, Throwable e)
+        {
+            // Submit an exception to the Exceptionless server
+            ExceptionlessHelper.SubmitException(e);
+
+            // Display error message for local debugging purposes
+            Debug.WriteLine(e);
+        }
+    }
+}
--- XamarinExceptionless/Droid/MainActivity.cs
+++ XamarinExceptionless/Droid/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Widget;
+using Java.Lang;
 
 namespace XamarinExceptionless.Droid
 {
@@ -15,6 +16,15 @@ namespace XamarinExceptionless.Droid
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Main);
+
+            // Initialize Exceptionless client before app startup
+            ExceptionlessHelper.Initialize("your_api_key_here");
+
+            // Set a the default exception handler to a custom one
+            if (!(Thread.DefaultUncaughtExceptionHandler is CustomExceptionHandler))
+            {
+                Thread.DefaultUncaughtExceptionHandler = new CustomExceptionHandler();
+            }
 
             Button button = FindViewById<Button>(Resource.Id.myButton);
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
