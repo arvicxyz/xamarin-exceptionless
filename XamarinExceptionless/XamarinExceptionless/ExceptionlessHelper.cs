1a418d4fe379834a417e9950bdbf03e6f4eac8e8
arvicxyz <dudeamirfahd@gmail.com>
2016-09-23 23:33:46 +0800

    Create ExceptionlessHelper class

---
+++ XamarinExceptionless/XamarinExceptionless/ExceptionlessHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Exceptionless;
+
+namespace XamarinExceptionless
+{
+    public static class ExceptionlessHelper
+    {
+        public static void Initialize(string apiKey)
+        {
+            ExceptionlessClient.Default.Startup(apiKey);
+        }
+
+        public static void SubmitException(Exception ex)
+        {
+            ex.ToExceptionless().Submit();
+        }
+    }
+}
